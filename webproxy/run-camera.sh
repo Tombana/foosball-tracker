0b5d952547c6d9cc30593213e90d19b35f1cc17c
Tom Bannink <tombannink@gmail.com>
2019-08-18 14:11:10 +0200

    Fix shaders in CMakeLists.txt

--- webproxy/run-camera.sh
+++ webproxy/run-camera.sh
@@ -5,5 +5,4 @@ pkill raspiballs 2>/dev/null
 fragments_path="/dev/shm/replay/fragments"
 mkdir -p $fragments_path
 
-exec ./raspiballs -o $fragments_path/out%04d.h264 -w 1280 -h 720 -fps 40 -t 0  -sg 100 -wr 100 -g 10 --ev 5 --glwin 450,700,640,480
-#exec /opt/vc/bin/raspivid -o $fragments_path/out%04d.h264 -w 1280 -h 720 -fps 60 -t 0  -sg 100 -wr 100 -g 10 --ev 5 -p 450,700,640,480
+exec ../build/raspiballs -o $fragments_path/out%04d.h264 -w 1280 -h 720 -fps 40 -t 0  -sg 100 -wr 100 -g 10 --ev 5 --glwin 450,700,640,360
